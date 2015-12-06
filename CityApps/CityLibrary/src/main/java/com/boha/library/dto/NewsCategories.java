2e36d5d92178a5f01c709acd41b639ac74fc6b81
Aubrey Malabie <malengatiger@gmail.com>
2015-12-06 14:01:55 +0200

    Removed old directory

--- CityApps/CityLibrary/src/main/java/com/boha/library/dto/NewsCategories.java
+++
@@ -1,7 +0,0 @@
-package com.boha.library.dto;
-
-/**
- * Created by aubreyM on 15/01/22.
- */
-public class NewsCategories {
-}
--- CityApps/CityLibrary/src/main/java/com/boha/library/dto/ProfileInfo.java
+++
@@ -1,7 +0,0 @@
-package com.boha.library.dto;
-
-/**
- * Created by aubreyM on 15/01/29.
- */
-public class ProfileInfo {
-}
