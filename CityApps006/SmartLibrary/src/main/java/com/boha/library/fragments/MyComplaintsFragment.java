0eb9ebc7bf112ce22d7ace0ff8b5c5318b42e84d
B-Chris <nkululekocgs@gmail.com>
2016-06-22 11:29:21 +0200

    filter complaints

--- CityApps006/SmartLibrary/src/main/java/com/boha/library/fragments/MyComplaintsFragment.java
+++ CityApps006/SmartLibrary/src/main/java/com/boha/library/fragments/MyComplaintsFragment.java
@@ -2,9 +2,14 @@ package com.boha.library.fragments;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,20 +18,28 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListPopupWindow;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.boha.library.R;
 import com.boha.library.activities.CityApplication;
+import com.boha.library.adapters.ComplaintCategoryPopupListAdapter;
 import com.boha.library.adapters.ComplaintListAdapter;
+import com.boha.library.adapters.ComplaintTypePopupListAdapter;
+import com.boha.library.dto.ComplaintCategoryDTO;
 import com.boha.library.dto.ComplaintDTO;
+import com.boha.library.dto.ComplaintTypeDTO;
 import com.boha.library.dto.ComplaintUpdateStatusDTO;
 import com.boha.library.dto.ProfileInfoDTO;
 import com.boha.library.dto.UserDTO;
 import com.boha.library.transfer.RequestDTO;
 import com.boha.library.transfer.ResponseDTO;
+import com.boha.library.util.CacheUtil;
 import com.boha.library.util.NetUtil;
 import com.boha.library.util.SharedUtil;
 import com.boha.library.util.Util;
@@ -35,6 +48,7 @@ import com.squareup.leakcanary.RefWatcher;
 import java.security.AccessController;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -66,8 +80,8 @@ public class MyComplaintsFragment extends Fragment implements PageFragment {
     List<String> stringList;
     Activity activity;
     View topView;
-    ImageView hero, noCompImage, searchIcon;
-    TextView txtNoComp;
+    ImageView hero, noCompImage, filterIcon;
+    TextView txtNoComp, FilterTxt, FilterTxt2;
     ProgressBar progressBar;
 
 
@@ -90,7 +104,7 @@ public class MyComplaintsFragment extends Fragment implements PageFragment {
         setFields();
 
         if (savedInstanceState != null) {
-            ResponseDTO w = (ResponseDTO)savedInstanceState.getSerializable("response");
+            ResponseDTO w = (ResponseDTO) savedInstanceState.getSerializable("response");
             if (w != null) {
                 response = w;
                 complaintList = response.getComplaintList();
@@ -98,18 +112,17 @@ public class MyComplaintsFragment extends Fragment implements PageFragment {
         }
 
         setList();
+        getCachedComplaintTypes();
         return view;
     }
 
     @Override
     public void onSaveInstanceState(Bundle b) {
-        b.putSerializable("response",response);
+        b.putSerializable("response", response);
         super.onSaveInstanceState(b);
     }
 
     ComplaintListAdapter adapter;
-    private AutoCompleteTextView autoComplete;
-    List <String> complaintNameList;
 
     private void setList() {
         if (complaintList == null) {
@@ -120,46 +133,6 @@ public class MyComplaintsFragment extends Fragment implements PageFragment {
             txtNoComp.setVisibility(View.GONE);
         }
         txtCount.setText("" + complaintList.size());
-
-        //complaints search
-      /*  if (complaintList.size() > 5) {
-            complaintNameList = new ArrayList<>(complaintList.size());
-            for (ComplaintDTO c : complaintList) {
-                complaintNameList.add(c.getCategory());
-            }
-            final ArrayAdapter<String> adapter = new ArrayAdapter<String>(ctx,
-                    R.layout.xxsimple_spinner_item, complaintNameList);
-            autoComplete.setAdapter(adapter);
-            autoComplete.setHint("Search for complaint");
-            autoComplete.setThreshold(1);
-            autoComplete.setVisibility(View.VISIBLE);
-
-            autoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
-
-                    hideKeyboard();
-                    listView.smoothScrollToPosition(i);
-                    autoComplete.setText("");
-
-                    int index = 0;
-                   // String name = adapter.getItem(i);
-                    for (ComplaintDTO com : complaintList) {
-                        if (com.getComplaintType().getCategoryName().equalsIgnoreCase(""));
-                        listView.smoothScrollToPosition(index);
-                        autoComplete.setText("");
-                        break;
-                    }
-                    index++;
-                }
-            });
-        } else {
-            autoComplete.setVisibility(View.GONE);
-        }*/
-
-
-
-
 
         adapter = new ComplaintListAdapter(ctx, R.layout.my_complaint_item, primaryDarkColor,
                 ComplaintListAdapter.MY_COMPLAINTS,
@@ -192,18 +165,6 @@ public class MyComplaintsFragment extends Fragment implements PageFragment {
 
     }
 
-    /*private void filterComplaints() {
-        List<ComplaintDTO> compList = new ArrayList<ComplaintDTO>();
-        ComplaintDTO complaint = new ComplaintDTO();
-        compList.add(complaint);
-        for (int i = 0; i < 5; i++) {
-            complaint = new ComplaintDTO();
-            compList.add(complaint);
-            complaint = new ComplaintDTO();
-
-        }
-    }*/
-
     private void underConstruction() {
         Util.showToast(ctx, getString(R.string.under_cons));
     }
@@ -214,6 +175,7 @@ public class MyComplaintsFragment extends Fragment implements PageFragment {
         complaintList = list;
         setList();
     }
+
     private void getCaseDetails(final String href, final int position) {
         Log.e(LOG, "##getCaseDetails, href: " + href);
         RequestDTO w = new RequestDTO(RequestDTO.GET_COMPLAINT_STATUS);
@@ -275,12 +237,26 @@ public class MyComplaintsFragment extends Fragment implements PageFragment {
         txtNoComp = (TextView) view.findViewById(R.id.FLC_noComplaints);
         noCompImage = (ImageView) view.findViewById(R.id.FLC_image);
         listView = (ListView) view.findViewById(R.id.FLC_listView);
-       /* autoComplete = (AutoCompleteTextView) view.findViewById(R.id.autocomplete_complaints);
-        if (autoComplete != null) {
-            hideKeyboard();
-        }
-        searchIcon = (ImageView) view.findViewById(R.id.FLC_SEARCH_ICON);
-        */
+        filterIcon = (ImageView) view.findViewById(R.id.filter_icon);
+        filterIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Util.flashOnce(view, 200, new Util.UtilAnimationListener() {
+                    @Override
+                    public void onAnimationEnded() {
+
+                        showComplaintCategoryPopup();
+
+                    }
+                });
+
+            }
+        });
+        FilterTxt = (TextView) view.findViewById(R.id.filter_Txt);
+        FilterTxt2 = (TextView) view.findViewById(R.id.filter_Txt2);
+
+
+
         txtTitle.setText(ctx.getString(R.string.my_complaints));
         txtSubTitle.setText(getString(R.string.history));
 
@@ -295,6 +271,7 @@ public class MyComplaintsFragment extends Fragment implements PageFragment {
         setNoCompImage();
         animateSomething();
     }
+
 
     Random random = new Random(System.currentTimeMillis());
 
@@ -335,13 +312,6 @@ public class MyComplaintsFragment extends Fragment implements PageFragment {
 //        }
     }
 
-    private void hideKeyboard() {
-            //ctx should be getContext
-        InputMethodManager imm = (InputMethodManager) ctx
-                .getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(autoComplete.getWindowToken(), 0);
-    }
-
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
@@ -366,6 +336,7 @@ public class MyComplaintsFragment extends Fragment implements PageFragment {
         RefWatcher refWatcher = CityApplication.getRefWatcher(getActivity());
         refWatcher.watch(this);
     }
+
 
     @Override
     public void animateSomething() {
@@ -410,7 +381,203 @@ public class MyComplaintsFragment extends Fragment implements PageFragment {
 
     public interface MyComplaintsListener {
         void setBusy(boolean busy);
-
         void onRefreshRequested(ComplaintDTO complaint);
     }
+
+    //complaint filter
+    List<ComplaintCategoryDTO> complaintCategoryList = new ArrayList<ComplaintCategoryDTO>();
+    ImageView search;
+    ComplaintCategoryDTO complaintCategory;
+    ComplaintTypeDTO complaintType;
+    ListPopupWindow categoryPopup, complaintPopup;
+    ComplaintCategoryPopupListAdapter complaintCategoryPopup;
+
+    public void showComplaintCategoryPopup() {
+        if (complaintPopup != null) {
+            complaintPopup.dismiss();
+        }
+
+        categoryPopup = new ListPopupWindow(getActivity());
+        LayoutInflater inf = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inf.inflate(R.layout.hero_image_popup, null);
+        TextView txt = (TextView) v.findViewById(R.id.HERO_caption);
+        txt.setText("Complaints Categories");
+        ImageView img = (ImageView) v.findViewById(R.id.HERO_image);
+        img.setImageDrawable(Util.getRandomBackgroundImage(ctx));
+
+        categoryPopup.setPromptView(v);
+        categoryPopup.setPromptPosition(ListPopupWindow.POSITION_PROMPT_ABOVE);
+        categoryPopup.setAdapter(new ComplaintCategoryPopupListAdapter(ctx,
+                R.layout.xspinner_item, complaintCategoryList, primaryDarkColor));
+        categoryPopup.setAnchorView(handle);
+        categoryPopup.setHorizontalOffset(Util.getPopupHorizontalOffset(getActivity()));
+        categoryPopup.setModal(true);
+        categoryPopup.setWidth(Util.getPopupWidth(getActivity()));
+        categoryPopup.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                categoryPopup.dismiss();
+                complaintCategory = complaintCategoryList.get(position);
+                Util.setComplaintCategoryIcon(complaintCategory.getComplaintCategoryName(), filterIcon, getActivity());
+                filterIcon.setColorFilter(primaryDarkColor, PorterDuff.Mode.SRC_IN);
+                FilterTxt.setText(complaintCategoryList.get(position).getComplaintCategoryName());
+                showComplaintTypePopup(complaintCategory.getComplaintTypeList());
+            }
+        });
+        categoryPopup.show();
+    }
+
+    public void showComplaintTypePopup(final List<ComplaintTypeDTO> list) {
+        if (categoryPopup != null) {
+            categoryPopup.dismiss();
+        }
+        complaintPopup = new ListPopupWindow(getActivity());
+        LayoutInflater inf = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inf.inflate(R.layout.hero_image_popup, null);
+        TextView txt = (TextView) v.findViewById(R.id.HERO_caption);
+        txt.setText("Complaints");
+        ImageView img = (ImageView) v.findViewById(R.id.HERO_image);
+        img.setImageDrawable(Util.getRandomBackgroundImage(ctx));
+
+        complaintPopup.setPromptView(v);
+        complaintPopup.setPromptPosition(ListPopupWindow.POSITION_PROMPT_ABOVE);
+        complaintPopup.setAdapter(new ComplaintTypePopupListAdapter(ctx,
+                R.layout.xspinner_item, list, primaryDarkColor));
+        complaintPopup.setAnchorView(handle);
+        complaintPopup.setHorizontalOffset(Util.getPopupHorizontalOffset(getActivity()));
+        complaintPopup.setModal(true);
+        complaintPopup.setWidth(Util.getPopupWidth(getActivity()));
+        complaintPopup.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                complaintPopup.dismiss();
+                complaintType = list.get(position);
+                Util.setComplaintTypeIcon(complaintType.getComplaintTypeName(), filterIcon, ctx);
+                filterIcon.setColorFilter(primaryDarkColor, PorterDuff.Mode.SRC_IN);
+                FilterTxt2.setText(list.get(position).getComplaintTypeName());
+                adapter.filter(FilterTxt2.getText().toString());
+            }
+        });
+        complaintPopup.show();
+    }
+
+    List<ComplaintTypeDTO> complaintTypeList;
+    private void getCachedComplaintTypes() {
+        CacheUtil.getCacheLoginData(ctx, new CacheUtil.CacheRetrievalListener() {
+            @Override
+            public void onCacheRetrieved(ResponseDTO response) {
+
+                if (response.getComplaintCategoryList() != null) {
+                    complaintCategoryList = response.getComplaintCategoryList();
+                }
+                if (response.getComplaintTypeList() != null) {
+                    complaintTypeList = response.getComplaintTypeList();
+                    stringList = new ArrayList<String>();
+                    for (ComplaintTypeDTO x : complaintTypeList) {
+                        stringList.add(x.getComplaintTypeName());
+                    }
+                }
+
+            }
+
+            @Override
+            public void onError() {
+
+            }
+        });
+    }
+
+
+    // filter class
+   /* public void filter() {
+       // FilterTxt2.getText().toString();// = charText.toLowerCase(Locale.getDefault());
+        complaintList.clear();
+        if (FilterTxt2.getText().toString().length() == 0) {
+            complaintList.addAll(response.getComplaintList());
+        }
+        else {
+            for (ComplaintDTO c : complaintList){
+
+                if (c.getComplaintType().getComplaintTypeName()
+                        .toLowerCase(Locale.getDefault()).contains(FilterTxt2.getText().toString())){
+                    complaintList.add(c);
+
+                }
+            }
+        }
+       // notifyDataSetChanged();
+    }
+
+    boolean isFound;
+    private void selectComplaintsByCategory() {
+        if (search.getContext().toString().isEmpty()) {
+            return;
+        }
+
+        //FilterTxt + FilterTxt2;
+
+        int index = 0;
+        String filterText = FilterTxt + FilterTxt2.getText().toString();
+        for (int i = 0; i < response.getComplaintList().size(); i++) {
+
+            ComplaintDTO text = response.getComplaintList().get(i);
+            if (text.getComplaintType().getComplaintTypeName().contains(filterText)) {
+                isFound = true;
+                break;
+            }
+            index++;
+        }
+        if (isFound) {
+            listView.setSelection(index);
+        } else {
+            Util.showToast(ctx, ctx.getString(R.string.complaint_not_found) + " " + filterText);
+        }
+
+
+        if (complaintList == null) {
+            complaintList = new ArrayList<>();
+        }
+        if (!complaintList.isEmpty()) {
+            noCompImage.setVisibility(View.GONE);
+            txtNoComp.setVisibility(View.GONE);
+        }
+        txtCount.setText("" + complaintList.size());
+
+        adapter = new ComplaintListAdapter(ctx, R.layout.my_complaint_item, primaryDarkColor,
+                ComplaintListAdapter.MY_COMPLAINTS,
+                complaintList, new ComplaintListAdapter.ComplaintListListener() {
+            @Override
+            public void onComplaintFollowRequested(ComplaintDTO complaint) {
+                underConstruction();
+            }
+
+            @Override
+            public void onComplaintStatusRequested(ComplaintDTO complaint, int position) {
+                if (complaint.getHref() == null) {
+                    listener.onRefreshRequested(complaint);
+                } else {
+                    getCaseDetails(complaint.getHref(), position);
+                }
+            }
+
+            @Override
+            public void onComplaintCameraRequested(ComplaintDTO complaint) {
+                underConstruction();
+            }
+
+            @Override
+            public void onComplaintImagesRequested(ComplaintDTO complaint) {
+                underConstruction();
+            }
+        });
+        listView.setAdapter(adapter);
+
+    }*/
+
+
+
 }
+
+
+
+
