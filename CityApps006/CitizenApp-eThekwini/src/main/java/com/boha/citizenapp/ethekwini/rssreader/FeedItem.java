4b0615c0896984a05e87ac07e69fc77c80b51dcc
B-Chris <nkululekocgs@gmail.com>
2016-08-28 17:00:24 +0200

    FeedItem class

---
+++ CityApps006/CitizenApp-eThekwini/src/main/java/com/boha/citizenapp/ethekwini/rssreader/FeedItem.java
@@ -0,0 +1,48 @@
+package com.boha.citizenapp.ethekwini.rssreader;
+
+/**
+ * Created by Nkululeko on 2016/08/24.
+ */
+public class FeedItem {
+    String title, link, description, pubDate, thumbnailUrl;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(String pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+}
