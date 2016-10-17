f9c9e855032567b99a63e6f8e13732377c04bd18
B-Chris <nkululekocgs@gmail.com>
2016-10-17 13:10:59 +0200

    FAQs class

---
+++ CityApps006/SmartLibrary/src/main/java/com/boha/library/util/FAQs.java
@@ -0,0 +1,61 @@
+package com.boha.library.util;
+
+/**
+ * Created by Nkululeko on 2016/10/05.
+ */
+
+public class FAQs {
+    public static final String ACCOUNTSPAYMENTS = "http://icsmnewsdev.oneconnectgroup.com/et/faq/AccountsPayments.html";
+
+    public static final String WATERSANITATION = "http://icsmnewsdev.oneconnectgroup.com/et/faq/WaterSanitation.html";
+
+    public static final String CLEANINGSOLIDWASTE = "http://icsmnewsdev.oneconnectgroup.com/et/faq/CleansingSolidWaste.html";
+
+    public static final String RATESTAXES = "http://icsmnewsdev.oneconnectgroup.com/et/faq/RatesTaxes.html";
+
+    public static final String BUILDINGPLANS = "http://icsmnewsdev.oneconnectgroup.com/et/faq/BuildingPlans.html";
+
+    public static final String ELECTRICITY = "http://icsmnewsdev.oneconnectgroup.com/et/faq/Electricity.html";
+
+    public static final String SOCIALSERVICES = "http://icsmnewsdev.oneconnectgroup.com/et/faq/SocialServices.html";
+
+    public static final String HEALTH = "http://icsmnewsdev.oneconnectgroup.com/et/faq/Health.html";
+
+    public static final String METROPOLICE = "http://icsmnewsdev.oneconnectgroup.com/et/faq/MetroPolice.html";
+
+    public static String getACCOUNTSPAYMENTS() {
+        return ACCOUNTSPAYMENTS;
+    }
+
+    public static String getWATERSANITATION() {
+        return WATERSANITATION;
+    }
+
+    public static String getCLEANINGSOLIDWASTE() {
+        return CLEANINGSOLIDWASTE;
+    }
+
+    public static String getRATESTAXES() {
+        return RATESTAXES;
+    }
+
+    public static String getBUILDINGPLANS() {
+        return BUILDINGPLANS;
+    }
+
+    public static String getELECTRICITY() {
+        return ELECTRICITY;
+    }
+
+    public static String getSOCIALSERVICES() {
+        return SOCIALSERVICES;
+    }
+
+    public static String getHEALTH() {
+        return HEALTH;
+    }
+
+    public static String getMETROPOLICE() {
+        return METROPOLICE;
+    }
+}
